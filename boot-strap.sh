4cc6ea3ef390dfd17064ae790bcb5da4a7fa22be
Shin Imai <linubshokunin@yahoo.com>
2019-11-09 21:03:55 +0000

    Added boot-strap.sh

---
+++ boot-strap.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Keep SSH connection
+echo "ClientAliveInterval 15" >> /etc/ssh/sshd_config
+
+#restart sshd
+systemctl restart sshd.service
+
+
+# Turn off swap
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+##################################
+# Install Docker CE
+## Set up the repository:
+### Install packages to allow apt to use a repository over HTTPS
+apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+### Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+### Add Docker apt repository.
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+## Install Docker CE.
+apt-get update && apt-get install docker-ce=5:18.09.9~3-0~ubuntu-xenial
+
+# Setup daemon.
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+##################################
+# Install Kubexxx
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+## Download docker images
+kubeadm config images pull
