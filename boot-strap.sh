7b755047c168a80ed89faae75e2090fc0e8435b6
Shin Imai <linubshokunin@yahoo.com>
2019-11-09 21:10:08 +0000

    Add -y. Removed sudo

--- boot-strap.sh
+++ boot-strap.sh
@@ -15,7 +15,7 @@ swapoff -a
 # Install Docker CE
 ## Set up the repository:
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 ### Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -27,7 +27,7 @@ add-apt-repository \
   stable"
 
 ## Install Docker CE.
-apt-get update && apt-get install docker-ce=5:18.09.9~3-0~ubuntu-xenial
+apt-get update && apt-get install -y docker-ce=5:18.09.9~3-0~ubuntu-xenial
 
 # Setup daemon.
 cat > /etc/docker/daemon.json <<EOF
@@ -49,14 +49,14 @@ systemctl restart docker
 
 ##################################
 # Install Kubexxx
-sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
 ## Download docker images
 kubeadm config images pull
