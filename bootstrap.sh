40b4d3fe5dbf2c0666656c2a44b75b23c8364be1
Shin Imai <linubshokunin@yahoo.com>
2019-12-10 10:32:48 +0000

    Added the wrong place. Fixed

--- bootstrap.sh
+++ bootstrap.sh
@@ -55,8 +55,8 @@ cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet=1.16.3-00 kubeadm=1.16.3-00 kubectl=1.16.3-00
+apt-get install -y kubelet=1.16.3-00 kubeadm=1.16.3-00 kubectl=1.16.3-00
+apt-mark hold kubelet kubeadm kubectl
 
 ## Download docker images
 kubeadm config images pull
